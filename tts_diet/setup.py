8f737852fd91eb536caca7dff6f929068a2e7a4b
Pete Cacioppi <Peter.Cacioppi@gmail.com>
2020-11-30 14:29:43 -0800

    fixed setup.py

--- tts_diet/setup.py
+++ tts_diet/setup.py
@@ -2,7 +2,7 @@ import tts_diet
 from setuptools import setup, find_packages
 
 setup(
-   name = 'self_serve_ticdat',
+   name = 'tts_diet',
    packages = find_packages(),
    version = tts_diet.__version__,
    author = 'Pete Cacioppi',
