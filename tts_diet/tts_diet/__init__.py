6f7b01aba24b49017acd22f9f7afc4cb19ba83ec
Pete Cacioppi <Peter.Cacioppi@gmail.com>
2020-12-20 17:08:25 -0800

    Move tts_diet to its own repo, where it belongs.

--- tts_diet/tts_diet/__init__.py
+++
@@ -1,3 +0,0 @@
-__version__ = "0.0.1"
-from tts_diet.tts_diet import input_schema, solution_schema, solve
-__all__ = ["input_schema", "solution_schema", "solve"]
--- tts_diet/tts_diet/__main__.py
+++
@@ -1,5 +0,0 @@
-# when run from the command line, will read/write json/xls/csv/db/sql/mdb files
-from ticdat import standard_main
-from tts_diet import input_schema, solution_schema, solve
-if __name__ == "__main__":
-    standard_main(input_schema, solution_schema, solve)
