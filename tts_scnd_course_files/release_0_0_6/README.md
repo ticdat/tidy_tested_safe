e9ee072677c33e2b4aa7720682bf3a4840194ba4
Pete Cacioppi <Peter.Cacioppi@gmail.com>
2021-06-23 19:00:46 -0700

    initial 0.0.6 information
    the forward conversion needs more information

---
+++ tts_scnd_course_files/release_0_0_6/README.md
@@ -0,0 +1,48 @@
+Files provided.
+
+ * `tts_scnd.py` - Only providing the new `input_schema` and `solution_schema`.  You will build `solve`.
+ * `tts_scnd_pd.py` - Same as `tts_scnd.py`, except using `PanDatFactory`.
+ 
+ Your repository for this release. 
+ 
+ * Implement the new `solve` function.
+   * Details in chapter 10.
+ * Forward convert the testing data. 
+   * Details on the forward conversion logic farther down.
+   * Both `LargeScale3Location.json` and `MIP_for_9_City_Example.json` should forward convert.
+ * Edit the `test_tts_scnd.py` appropriately.
+   * The `try/except` test you added in release 0.0.2 is no longer relevant, as the lane tables aren't
+   bi-directionally safe.
+   * **Don't** retain the test that the `MIP_for_9_City_Example` total cost solution is the 
+   same even after removing "Atlanta" to "Charlotte". It's no longer relevant since there the warehouse 
+   to customer tables aren't bi-directionally safe.
+   * All the other tests carry forward in a straightforward way.
+     * The test you added in release 0.0.3 that involved removing both "Atlanta" to "Charlotte" and 
+     "Charlotte" to "Atlanta" for `MIP_for_9_City_Example` is still relevant. If there is no way
+     for the Atlanta customer to be assigned to the Charlotte warehouse, nor vice-versa, then the solution 
+      average service distance should be larger than `1.01*5.5277e8` divided by the total demand.
+   * Add the sequence of tests involving the `100_big_cities.json` file detailed below.
+   
+### Forward conversion of testing data 
+
+The two input schema changes significantly between 0.0.5 and 0.0.6. Technically, 0.0.5 `solve`
+doesn't fully reduce to the 0.0.6 `solve`, because 0.0.5 can support bi-direction shipment information
+and 0.0.6 can't. That said, we can bear this in mind to make sure both of the 0.0.5 data sets
+forward convert accurately. 
+
+Here is a summary of the 0.0.5->0.0.6 forward conversion logic.
+* The "Number of Centroids" parameter in 0.0.5 data is a "Number of Warehouses" parameter for 0.0.6.
+* The 0.0.6 `dat` object gets a single product record. 
+  * For simplicity, lets assume this is product `"P"`.
+  * Product `"P"` needs a Warehouse Volume of 1.
+* For each record of the 0.0.5 cities table...
+   * Create a plant with the same name.
+     * Create a supply record for the plant you just created and product `"P"` with a supply of 
+     infinity.
+   * Create a warehouse with the same name, a Fixed Cost of zero, and the same Max Assignment Capacity.
+   * Create a customer with the same name.
+     * Create a demand record for the customer you just created and product `"P"` with a demand the
+   same as the cities record.
+
+   
+`
---
+++ tts_scnd_course_files/release_0_0_6/tts_scnd_pd.py
@@ -0,0 +1,64 @@
+from ticdat import PanDatFactory
+
+input_schema = PanDatFactory(plants=[["Name"],[]],
+                             warehouses=[["Name"], ["Max Assignment Capacity", "Fixed Cost"]],
+                             customers=[["Name"], []],
+                             products=[["Name"], ["Warehouse Volume"]],
+                             demand=[["Customer", "Product"], ["Demand"]],
+                             supply=[["Plant", "Product"], ["Supply"]],
+                             plant_to_warehouse_costs=[["Plant", "Warehouse", "Product"], ["Cost"]],
+                             warehouse_to_customer_costs=[["Warehouse", "Customer", "Product"], ["Cost"]],
+                             warehouse_to_customer_distances=[["Warehouse", "Customer"], ["Distance"]],
+                             parameters=[["Parameter"], ["Value"]])
+
+input_schema.add_parameter("Number of Warehouses", default_value=4, inclusive_min=False, inclusive_max=False, min=0,
+                            max=float("inf"), must_be_int=True)
+input_schema.add_parameter("High Service Distance", default_value=0, inclusive_min=True, inclusive_max=True, min=0,
+                            max=float("inf"), must_be_int=False)
+input_schema.add_parameter("Maximum Average Service Distance", default_value=float("inf"), inclusive_min=True,
+                           inclusive_max=True, min=0, max=float("inf"), must_be_int=False)
+input_schema.add_parameter("Minimum Percent High Service Demand", default_value=0, inclusive_min=True,
+                           inclusive_max=True, min=0, max=100, must_be_int=False)
+input_schema.add_parameter("Maximum Individual Service Distance", default_value=float("inf"), inclusive_min=False,
+                           inclusive_max=True, min=0, max=float("inf"), must_be_int=False)
+input_schema.add_parameter("Maximum Total Cost", default_value=float("inf"), inclusive_min=True,
+                           inclusive_max=True, min=0, max=float("inf"), must_be_int=False)
+input_schema.add_parameter("Objective", "Minimize Average Service Distance",
+    strings_allowed=["Minimize Average Service Distance", "Maximize Percent High Service Demand", "Minimize Total Cost"],
+    number_allowed=False)
+input_schema.set_data_type("warehouses", "Fixed Cost", min=0, max=float("inf"), inclusive_min=True, inclusive_max=False)
+input_schema.set_data_type("warehouses", "Max Assignment Capacity", min=0, max=float("inf"),
+                           inclusive_min=True, inclusive_max=True)
+input_schema.set_default_value("warehouses", "Max Assignment Capacity", float("inf"))
+input_schema.set_data_type("products", "Warehouse Volume", min=0, max=float("inf"),
+                           inclusive_min=False, inclusive_max=False)
+input_schema.set_default_value("products", "Warehouse Volume", 1)
+input_schema.set_data_type("demand", "Demand", min=0, max=float("inf"), inclusive_min=True, inclusive_max=False)
+input_schema.set_data_type("supply", "Supply", min=0, max=float("inf"), inclusive_min=True, inclusive_max=True)
+input_schema.set_default_value("supply", "Supply", float("inf"))
+input_schema.set_data_type("plant_to_warehouse_costs", "Cost", min=0, max=float("inf"), inclusive_min=True,
+                           inclusive_max=False)
+input_schema.set_data_type("warehouse_to_customer_costs", "Cost", min=0, max=float("inf"), inclusive_min=True,
+                           inclusive_max=False)
+input_schema.set_data_type("warehouse_to_customer_distances", "Distance", min=0, max=float("inf"), inclusive_min=True,
+                           inclusive_max=False)
+input_schema.add_foreign_key("demand", "customers", ["Customer", "Name"])
+input_schema.add_foreign_key("demand", "products", ["Product", "Name"])
+input_schema.add_foreign_key("supply", "plants", ["Plant", "Name"])
+input_schema.add_foreign_key("supply", "products", ["Product", "Name"])
+input_schema.add_foreign_key("plant_to_warehouse_costs", "plants", ["Plant", "Name"])
+input_schema.add_foreign_key("plant_to_warehouse_costs", "warehouses", ["Warehouse", "Name"])
+input_schema.add_foreign_key("plant_to_warehouse_costs", "products", ["Product", "Name"])
+input_schema.add_foreign_key("warehouse_to_customer_distances", "customers", ["Customer", "Name"])
+input_schema.add_foreign_key("warehouse_to_customer_distances", "warehouses", ["Warehouse", "Name"])
+input_schema.add_foreign_key("warehouse_to_customer_costs", "products", ["Product", "Name"])
+input_schema.add_foreign_key("warehouse_to_customer_costs", "warehouse_to_customer_distances",
+                             [["Warehouse", "Warehouse"], ["Customer", "Customer"]])
+
+solution_schema = PanDatFactory(warehouses_opened=[['Warehouse'], ["Fixed Cost", "Assignment Volume"]],
+                                plant_to_warehouse_shipments=[["Plant", "Warehouse", "Product"],
+                                                              ["Units Shipped", "Shipment Cost"]],
+                                warehouse_to_customer_shipments=[["Warehouse", "Customer", "Product"],
+                                                                 ["Units Shipped", "Shipment Cost"]],
+                                production=[["Plant", "Product"], ["Units Produced"]],
+                                parameters=[["Parameter"], ["Value"]])
